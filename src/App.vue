5df01bfa303d0ed96a6ee3f2970d97eec8880b1a
francescofalcone24 <cicciofalcon97@gmail.com>
2024-10-24 20:01:28 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import VueCal from 'vue-cal';
+import 'vue-cal/dist/vuecal.css';
+
+export default {
+  components: { VueCal },
+  data() {
+  },
+}
+
+</script>
+
+<template>
+  <div class="container">
+    <vue-cal :time-from="1 * 60" :time-to="24 * 60" :time-step="60" hide-weekends show-time-in-cells>
+    </vue-cal>
+  </div>
+
+
+</template>
+
+<style scoped></style>
